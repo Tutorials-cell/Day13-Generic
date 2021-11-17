2011fb9548ce293067448a66c4f891ab076f3bf5
Bahulya <sarvalabahulya456@gmail.com>
2021-11-17 18:21:16 +0530

    WuthOutGeneric

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxThree obj1 = new MaxThree();
+            obj1.ReturnMax(25, 56, 11);
+        }
+    }
+}
