13201c690a1cadcaf0a1e2317abdee8b13f91027
Bahulya <sarvalabahulya456@gmail.com>
2021-11-17 18:31:50 +0530

    floats

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MaxOfThreeFloats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           ThreeFloats obj1 = new ThreeFloats();
+            obj1.ReturnMax(11.4, 78, 45.9);
+        }
+    }
+}
---
+++ ThreeFloats.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxOfThreeFloats
+{
+    class ThreeFloats
+    {
+    }
+}
